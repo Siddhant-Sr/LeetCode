24f83588d3bc8e27a1919ef92e2c30943d03b805
Siddhant Srivastava <67307537+Siddhant-Sr@users.noreply.github.com>
2022-03-18 12:51:40 +0530

    Time: 48 ms (70.65%), Space: 17.8 MB (49.28%) - LeetHub

--- 143-reorder-list/143-reorder-list.cpp
+++ 143-reorder-list/143-reorder-list.cpp
@@ -10,42 +10,76 @@
  */
 class Solution {
 public:
-    ListNode* reverse(ListNode *head){
-        if(head==NULL)
-            return head;
+//     ListNode* reverse(ListNode *head){
+//         if(head==NULL)
+//             return head;
         
-        if(head->next==NULL)
-            return head;
+//         if(head->next==NULL)
+//             return head;
         
-        ListNode*prev=NULL, *curr=head, *forward = head;
-        while(curr){
-            forward = curr->next;
-            curr->next = prev;
-            prev=curr;
-            curr= forward;
-        }
-        return prev;
-    }
+//         ListNode*prev=NULL, *curr=head, *forward = head;
+//         while(curr){
+//             forward = curr->next;
+//             curr->next = prev;
+//             prev=curr;
+//             curr= forward;
+//         }
+//         return prev;
+//     }
     void reorderList(ListNode* head) {
-                if (!head) return;
+//                 if (!head) return;
 
-        ListNode * slow = head, *fast=head;
-        while(fast && fast->next){
-            slow = slow->next;
-            fast=fast->next->next;
-        }
-        ListNode*temp = slow->next;
-        slow->next=NULL; 
-        ListNode*temp2 = reverse(temp);
+//         ListNode * slow = head, *fast=head;
+//         while(fast && fast->next){
+//             slow = slow->next;
+//             fast=fast->next->next;
+//         }
+//         ListNode*temp = slow->next;
+//         slow->next=NULL; 
+//         ListNode*temp2 = reverse(temp);
         
-    while(head && temp2){
-        fast =head->next;
-        temp = temp2->next;
-        head->next = temp2;
-            temp2->next = fast;
-            head = fast;
-            temp2 = temp;
-    }
+//     while(head && temp2){
+//         fast =head->next;
+//         temp = temp2->next;
+//         head->next = temp2;
+//             temp2->next = fast;
+//             head = fast;
+//             temp2 = temp;
+//     }
+              // base case : linkedlist is empty
+        if (!head) return;
+        
+        // finding the middle with the help of two pointer approach
+        ListNode *tmp = head, *half = head, *prev = NULL;
+        while (tmp->next && tmp->next->next) {
+            tmp = tmp->next->next;
+            half = half->next;
+        }
+        
+        // adding one bit in case of lists with even length
+        if (tmp->next) half = half->next;
+        
+        // Now reverse the second half
+        while (half) {
+            tmp = half->next;
+            half->next = prev;
+            prev = half;
+            half = tmp;
+        }
+        half = prev;
+        
+        // After reversing the second half, let's merge both the halfes
+        while (head && half) {
+            tmp = head->next;
+            prev = half->next;
+            head->next = half;
+            half->next = tmp;
+            head = tmp;
+            half = prev;
+        }
+        
+        // Base case : closing when we had even length arrays
+        if (head && head->next) head->next->next = NULL;
         
     }
 };
